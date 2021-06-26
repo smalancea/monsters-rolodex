4587e635a16b10947fde9965ef9ae9accfb0a897
smalancea <82141196+smalancea@users.noreply.github.com>
2021-06-26 16:36:14 +0300

    Naming the 3 Monsters

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankenstein'
+        },
+        {
+          name: 'Dracula'
+        },
+        {
+          name: 'Zombie'
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>hello sergiu</p>
-          <button>Change Text</button>
-        </header>
+        {this.state.monsters.map(monster => (
+        <h1> {monster.name} </h1>
+        ))}
       </div>
     );
   }

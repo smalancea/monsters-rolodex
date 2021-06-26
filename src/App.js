14db522abb5f3a58f30e16cfaca5d7a45cbc9e57
smalancea <82141196+smalancea@users.noreply.github.com>
2021-06-26 18:06:12 +0300

    Fetching Content (monsters name)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,25 +6,21 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: 'Dracula'
-        },
-        {
-          name: 'Zombie'
-        }
-      ]
+      monsters: []
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
   }
 
   render() {
     return (
       <div className="App">
         {this.state.monsters.map(monster => (
-        <h1> {monster.name} </h1>
+        <h1 key={monster.id}> {monster.name} </h1>
         ))}
       </div>
     );
